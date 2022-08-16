46bf272f9883b39e5dea5d837e1ca859fcf99df9
Gal Tamir <gal.tamir@gmail.com>
2022-08-17 00:23:42 +0300

    server README.md file as default

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const { processPdfByUrl } = require('./processPdfByUrl.js');
+var fs = require('fs');
+const path = require('path');
+const { marked } = require('marked');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
+// Display README.md file for server's root
+app.get('/', async (req, res, next) => {
+    var file = fs.readFileSync(path.join(__dirname, '/README.md'), 'utf8');
+    res.send(marked(file.toString()));
+})
+
 app.get('/parser/', async (req, res, next) => {
     try {
 
         // Check for query param: pdf
-        if (!req.query.pdf){
+        if (!req.query.pdf) {
             res.status(400).send('Error:  Missing ?pdf= parameter');
-            return; 
+            return;
         }
 
         const pdfUri = req.query.pdf;
